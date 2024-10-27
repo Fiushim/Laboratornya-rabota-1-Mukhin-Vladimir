c8f76d95ffbef0afafa1cc2ddef95da46b893a97
Fiushim <51487722+Fiushim@users.noreply.github.com>
2024-10-27 21:28:16 +0300

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,21 @@
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+months = 10  # Количество месяцев, которое планируется протянуть без долгов
+increase = 0.03  # Ежемесячный рост цен
+
+# TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
+
+count = 0
+money_capital = 0
+while count < months:
+    count += 1
+
+    money = spend - salary
+
+    spend *= (1 + increase)
+
+    money_capital += money
+
+
+
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", round(money_capital))
